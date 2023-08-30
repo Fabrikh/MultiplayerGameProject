93169d6c5b3ffd26330ca57120b82eb0f867d7e5
Lorenzo Soannini <lorenzosoannini@hotmail.it>
2023-08-30 18:35:22 +0200

    Updated Rng with secure random rumber generator instead of the weak random python module

--- p2pWebSocket/Server/chat/rng/app.py
+++ p2pWebSocket/Server/chat/rng/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify
-import random
+import secrets
 
 app = Flask(__name__)
 
 @app.route('/api/random')
 def generate_random_number():
-    random_number = random.randint(2, 6)
-    return jsonify({"random_number": random_number})
+    secret_random_number = secrets.randbelow(6) + 1
+    return jsonify({"random_number": secret_random_number})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3999)
